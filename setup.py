2a31b23cae4ac1ae755cee3b44ff95df8e269e3d
David Kundih <kundihdavid@gmail.com>
2021-09-25 17:29:14 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: Apache Software License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='alunari',
+  version='1.3.1',
+  description='Data science and Machine learning package.',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(), 
+  author='David Kundih',
+  author_email='kundihdavid@gmail.com',
+  url='http://github.com/dkundih/alunari',
+  license='Apache Software License', 
+  classifiers=classifiers,
+  keywords='data science, machine learning, artificial intelligence, AI, alunari',
+  packages=find_packages(),
+  install_requires=['pandas == 1.2.3', 'numpy == 1.19.5', 'matplotlib == 3.4.3', 'UNIN >= 0.0.2']
+)
