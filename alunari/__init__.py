dc14406bf7efb13f6758eb9957e0b6ee8479c43d
David Kundih <kundihdavid@gmail.com>
2021-09-27 11:31:28 +0200

    Add files via upload

--- alunari/__init__.py
+++ alunari/__init__.py
@@ -3,3 +3,7 @@ from alunari.eoq import eoq
 from alunari.essence import essence
 from alunari.misc.global_functions import *
 from alunari.montecarlo import montecarlo
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import numpy as np
