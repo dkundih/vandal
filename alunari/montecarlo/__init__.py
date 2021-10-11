934201927ab4c38f7c3bfefdf0efbf6b5e68978c
David Kundih <kundihdavid@gmail.com>
2021-10-11 23:25:20 +0200

    Add files via upload

--- alunari/montecarlo/__init__.py
+++ alunari/montecarlo/__init__.py
@@ -1,7 +1,9 @@
-from alunari.montecarlo import montecarlo
-from alunari.essence.essence import random_value
-from alunari.misc.global_functions import *
+from vandal.montecarlo import montecarlo
+from vandal.essence.essence import random_value
+from vandal.misc.global_functions import *
 import random
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+import datetime
