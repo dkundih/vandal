60497be835eb1fab30da41eee215196f95afee1f
David Kundih <kundihdavid@gmail.com>
2021-10-12 16:28:37 +0200

    Add files via upload

---
+++ CHANGELOG.md
@@ -0,0 +1,28 @@
+CHANGELOG
+==========
+
+0.0.1 (03/10/2021)
+- initial release.
+
+0.0.2 (03/10/2021)
+- vandal replaces the functionality of the currently discarded alunari python package.
+
+1.1.0 (11/10/2021)
+- redefined code of hist() function.
+- added event log that tracks the execution time and duration of functions.
+
+1.1.1 (11/10/2021)
+- log tracking now applies on all relevant class functions.
+
+2.0.0 (11/10/2021)
+- vandal transcedents alunari versions by becoming v2+
+
+2.0.1 (12/10/2021)
+- minor tweaks to CHANGELOG and README
+- .help() now properly shows requirements of .get_logs() function.
+
+2.0.2+ (12/10/2021)
+- photo added to the header.
+
+2.0.3 (12/10/2021)
+- confirmed stabile version after test.
--- setup.py
+++ setup.py
@@ -5,13 +5,24 @@ classifiers = [
   'Intended Audience :: Education',
   'Operating System :: Microsoft :: Windows :: Windows 10',
   'License :: OSI Approved :: Apache Software License',
-  'Programming Language :: Python :: 3'
+  'Programming Language :: Python',
+   'Programming Language :: Python :: 3 :: Only',
+   'Programming Language :: Python :: 3.5',
+   'Programming Language :: Python :: 3.6',
+   'Programming Language :: Python :: 3.7',
+   'Programming Language :: Python :: 3.8',
+   'Programming Language :: Python :: 3.9',
+   'Programming Language :: Python :: 3.10',
+    'Topic :: Database',
+    'Topic :: Education',
+    'Topic :: Office/Business'
 ]
  
 setup(
   name='vandal',
-  version='insert_the_version',
+  version='2.0.3',
   description='Data science and Machine learning package.',
+  long_description_content_type='text/markdown',
   long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(), 
   author='David Kundih',
   author_email='kundihdavid@gmail.com',
